d2aee81c1505f0d6ba4e84959c36b23ffc8026b6
mukilanb28 <37461574+mukilanb28@users.noreply.github.com>
2022-10-18 15:52:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  console.log(`\n RQUEST TRIGGERED --- ${req.connection.remoteAddress} -- ${req.socket.remoteAddress} -- ${req.headers['x-forwarded-for']}`)
-  try{
-    for(let i in req.headers){
-        console.log(`${i} : ${req.headers[i]}`);
-    }
-  }catch{
-
-  }
-  console.log("\n ->", req.headers.socket.handshake.headers['x-forwarded-for'].split(',')[0])
-
+  
   res.end('Hello, World! ' + req.connection.remoteAddress + " && " + req.socket.remoteAddress + " && " + req.headers['x-forwarded-for']);
 }
 
