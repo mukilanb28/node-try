09f2c25b6cca4b6df5e46e7ff343cbc0764a45d3
mukilan.balasubramaniyan <p5telvaui6oivzmndipsqpfywswcaps56ykvjr5golml7l5m6grq>
2024-02-22 23:44:18 +0530

    Ts commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api75cb8d13": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "talebackendead527f0": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
