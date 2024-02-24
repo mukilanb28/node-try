be74540c7eb39e9245894c20f45bb10b9015f5fe
mukilan.balasubramaniyan <p5telvaui6oivzmndipsqpfywswcaps56ykvjr5golml7l5m6grq>
2024-02-24 08:11:53 +0530

    Sample response

--- amplify/backend/function/talebackendead527f0/src/index.js
+++ amplify/backend/function/talebackendead527f0/src/index.js
@@ -1,6 +1,16 @@
 exports.handler = (event, context) => {
+  var response = {
+      "statusCode": 200,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "isBase64Encoded": false,
+      "multiValueHeaders": { 
+        "X-Custom-Header": ["My value", "My other value"],
+      },
+      "body": "{\n  \"TotalCodeSize\": 104330022,\n  \"FunctionCount\": 26\n}"
+    };
+    
   console.log(`EVENT--: ${JSON.stringify(event)}`);
-  return new Promise((resolve,reject)=>{
-    resolve("SUCCESS!!!!@@!!")
-  });
+  return response;
 };
