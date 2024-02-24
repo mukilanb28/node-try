6e126705a814e617cfb740487fef6529e6706c42
mukilan.balasubramaniyan <p5telvaui6oivzmndipsqpfywswcaps56ykvjr5golml7l5m6grq>
2024-02-24 09:27:30 +0530

    Cp command included

--- amplify/backend/function/talebackendead527f0/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const handler = (event, context, callback) => {
-    console.log(`Event^^ ${JSON.stringify(event)}`);
-    const concatKey = `${event.key1} ${event.key2} ${event.key3}`;
-    callback(null, concatKey);
-};
-exports.handler = handler;
